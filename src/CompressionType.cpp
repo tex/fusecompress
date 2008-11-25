a19a2cacc8db42ab1fe9b37d3a06e48f7248337e
Milan Svoboda <milan.svoboda@centrum.cz>
2008-11-25 16:57:26 +0100

    Make all previously required compression libraries optional only.

--- src/CompressionType.cpp
+++ src/CompressionType.cpp
@@ -6,17 +6,18 @@
 #include <boost/iostreams/traits.hpp>
 
 #include "CompressionType.hpp"
+#include "config.h"
 
 #include <iostream>
 
 CompressionType::CompressionType() :
-#ifdef HAVE_ZLIB
+#ifdef HAVE_LIBZ
 	m_Type (ZLIB)
-#elif HAVE_LZO
+#elif HAVE_LIBLZO2
 	m_Type(LZO)
-#elif HAVE_BZIP2
+#elif HAVE_LIBBZ2
 	m_Type(BZIP2)
-#elif HAVE_LZMA
+#elif HAVE_LIBLZMA
 	m_Type(LZMA)
 #else
 	m_Type(NONE)
@@ -57,12 +58,12 @@ void CompressionType::push(io::filtering_stream<io::output>& fs) const
 	switch (m_Type) {
 	case NONE:
 		break;
-#ifdef HAVE_ZLIB
+#ifdef HAVE_LIBZ
 	case ZLIB:
 		fs.push(io::zlib_compressor(io::zlib_params(9, io::zlib::deflated, 15, 8, io::zlib::default_strategy, true)));
 		break;
 #endif
-#ifdef HAVE_BZIP2
+#ifdef HAVE_LIBBZ2
 	case BZIP2:
 		fs.push(io::bzip2_compressor());
 		break;
@@ -70,35 +71,38 @@ void CompressionType::push(io::filtering_stream<io::output>& fs) const
 	case XOR:
 		fs.push(io::xor_filter('2'));
 		break;
-#ifdef HAVE_LZO
+#ifdef HAVE_LIBLZO2
 	case LZO:
 		fs.push(io::lzo_compressor());
 		break;
 #endif
-#ifdef HAVE_LZMA
+#ifdef HAVE_LIBLZMA
 	case LZMA:
 		fs.push(io::lzma_compressor());
 		break;
 #endif
 	default:
-		// This should never happen on OUTPUT filtering stream.
-		//
+	{
+		// This shall never happen on OUTPUT filtering stream.
+
 		assert(false);
 		break;
 	}
+	}
 }
+
 template<>
 void CompressionType::push(io::filtering_stream<io::input>& fs) const
 {
 	switch (m_Type) {
 	case NONE:
 		break;
-#ifdef HAVE_ZLIB
+#ifdef HAVE_LIBZ
 	case ZLIB:
 		fs.push(io::zlib_decompressor(io::zlib_params(9, io::zlib::deflated, 15, 8, io::zlib::default_strategy, true)));
 		break;
 #endif
-#ifdef HAVE_BZIP2
+#ifdef HAVE_LIBBZ2
 	case BZIP2:
 		fs.push(io::bzip2_decompressor());
 		break;
@@ -106,19 +110,25 @@ void CompressionType::push(io::filtering_stream<io::input>& fs) const
 	case XOR:
 		fs.push(io::xor_filter('2'));
 		break;
-#ifdef HAVE_LZO
+#ifdef HAVE_LIBLZO2
 	case LZO:
 		fs.push(io::lzo_decompressor());
 		break;
 #endif
-#ifdef HAVE_LZMA
+#ifdef HAVE_LIBLZMA
 	case LZMA:
 		fs.push(io::lzma_decompressor());
 		break;
 #endif
 	default:
+	{
+		// If input file is using unsopported version
+		// throw an exception and FuseCompress will
+		// process such file as uncompressed.
+
 		throw BOOST_IOSTREAMS_FAILURE("unsupported compression type");
 		break;
+	}
 	}
 }
 
@@ -126,21 +136,21 @@ bool CompressionType::parseType(std::string type)
 {
 	if (type == "none")
 		m_Type = NONE;
-#ifdef HAVE_ZLIB
+#ifdef HAVE_LIBZ
 	else if (type == "zlib")
 		m_Type = ZLIB;
 #endif
-#ifdef HAVE_BZIP2
+#ifdef HAVE_LIBBZ2
 	else if (type == "bzip2")
 		m_Type = BZIP2;
 #endif
 	else if (type == "xor")
 		m_Type = XOR;
-#ifdef HAVE_LZO
+#ifdef HAVE_LIBLZO2
 	else if (type == "lzo")
 		m_Type = LZO;
 #endif
-#ifdef LZMA
+#ifdef HAVE_LIBLZMA
 	else if (type == "lzma")
 		m_Type = LZMA;
 #endif
