7a407ebddfe594be2b6921fad36f6502fa4182fc
Milan Svoboda <milan.svoboda@centrum.cz>
2008-12-25 09:48:58 +0100

    Remove duplicate definition of function.

--- src/CompressionType.cpp
+++ src/CompressionType.cpp
@@ -37,24 +37,6 @@ void CompressionType::printAllSupportedMethods(std::ostream& os)
 	os << "xor";
 }
 
-void CompressionType::printAllSupportedMethods(std::ostream& os)
-{
-	os << "none, ";
-#ifdef HAVE_LIBZ
-	os << "zlib, ";
-#endif
-#ifdef HAVE_LIBLZO2
-	os << "lzo, ";
-#endif
-#ifdef HAVE_LIBBZ2
-	os << "bzip2, ";
-#endif
-#ifdef HAVE_LIBLZMA
-	os << "lzma, ";
-#endif
-	os << "xor";
-}
-
 std::ostream& operator<<(std::ostream& os, const CompressionType& rObj)
 {
 	std::string name;
